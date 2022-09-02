28a35f69e1af80bdf74a6d6a5617f82bc53bd1cf
BigSuggs72 <msofi72@gmail.com>
2022-09-01 20:43:48 -0400

    update

--- models/Book.js
+++ models/Book.js
@@ -1,19 +1,38 @@
 const mongoose = require('mongoose')
 
 const BookSchema = new mongoose.Schema({
-  book: {
-    type: String,
-    required: true,
-  },
-  completed: {
-    type: Boolean,
-    required: true,
-  },
   userId: {
     type: String,
     required: true
-  }
+  }, 
+  author: {
+    type: String,
+    required: true
+  },
+  title: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: false
+  },
+  subjects: {
+    type: String,
+    required: true
+  },
+  notes: {
+    type: String,
+    required: false
+  },
+  checkedout: {
+    type: Boolean,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+}
 })
 
-module.exports = mongoose.model('Book', BookSchema)
-
+module.exports = mongoose.model('Book', BookSchema, 'books')
