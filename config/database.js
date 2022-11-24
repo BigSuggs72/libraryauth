ee044ba50c4b92a1caac4f2446110b58e1910630
BigSuggs72 <msofi72@gmail.com>
2022-11-24 08:42:51 -0500

    update

--- config/database.js
+++ config/database.js
@@ -2,14 +2,7 @@ const mongoose = require('mongoose')
 
 const connectDB = async () => {
   try {
-    // const conn = await mongoose.connect(process.env.DB_STRING, {
-    //   useNewUrlParser: true,
-    //   useUnifiedTopology: true,
-    //   useFindAndModify: false,
-    //   useCreateIndex: true
-    // })
-
-    const conn = await mongoose.connect(process.env.MONGO_URL, {
+    const conn = await mongoose.connect(process.env.DB_STRING, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useFindAndModify: false,
